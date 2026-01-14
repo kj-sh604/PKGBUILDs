03091173aef256ebf03166fd12887c7e4d4b5d9c
kj_sh604 <43.splash@gmail.com>
2026-01-13 23:37:59 -0500

    refactor: remove a good chunk of the `Inherits=`
    apparently this affects performance of GTK4 apps lol

--- elementary-xfce-icons-inherits-hook-kj_sh604/elementary-xfce-icons-inherits.sh
+++ elementary-xfce-icons-inherits-hook-kj_sh604/elementary-xfce-icons-inherits.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 theme_file="/usr/share/icons/elementary-xfce/index.theme"
-new_inherits="Inherits=elementary,Yaru-wartybrown-dark,Yaru-wartybrown,Yaru-dark,Yaru,Papirus,kora,Mint-Y,Mint-X,Moka,Faba,Humanity,gnome,Adwaita,breeze,Tango,hicolor"
+new_inherits="Inherits=elementary,Adwaita,AdwaitaLegacy,hicolor,gnome"
 
 if [[ -f "$theme_file" ]]; then
     sed -i "s/^Inherits=.*/$new_inherits/" "$theme_file"
